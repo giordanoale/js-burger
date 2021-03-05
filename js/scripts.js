9c64243b3925686a9f86284f33eb106d24081a08
giordanoale <a.giordano89@gmail.com>
2021-03-05 16:35:16 +0100

    Aggiunto controllo del prezzo degli ingredienti

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var calculate = document.getElementById("calculate");
 var burgerName = document.getElementById("burger-name-input");
+var ingredients = document.getElementsByClassName("add-ingredient");
 
 // Evento click del bottone
 calculate.addEventListener("click",
@@ -8,6 +9,15 @@ calculate.addEventListener("click",
     // controllo se è stato inserito il nome del panino
     if (burgerName.value != ""){
 
+      // aggiungo il prezzo degli eventuali ingredienti al totale
+      var sum = 50;
+      for (var i = 0; i < ingredients.length; i++) {
+        if (ingredients[i].checked == true) {
+          sum += parseInt(ingredients[i].value);
+        }
+      }
+
+      if ()
     } else {
       alert("Inserisci il nome del panino!");
 
